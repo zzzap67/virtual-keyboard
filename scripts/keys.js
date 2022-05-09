5cf8aaef845adc84da4961ff9e0052c1273005e4
zzzap67 <zzzap67@gmail.com>
2022-05-09 07:52:34 +0300

    feat: keyboard script

---
+++ scripts/keys.js
@@ -0,0 +1,192 @@
+export default [
+  {
+    row: 1, code: 'Backquote', spec: false, ru: { capstyle: true, sml: 'ё', big: 'Ё' }, en: { capstyle: false, sml: '`', big: '~' },
+  },
+  {
+    row: 1, code: 'Digit1', spec: false, ru: { capstyle: false, sml: '1', big: '!' }, en: { capstyle: false, sml: '1', big: '!' },
+  },
+  {
+    row: 1, code: 'Digit2', spec: false, ru: { capstyle: false, sml: '2', big: '\'' }, en: { capstyle: false, sml: '2', big: '@' },
+  },
+  {
+    row: 1, code: 'Digit3', spec: false, ru: { capstyle: false, sml: '3', big: '№' }, en: { capstyle: false, sml: '3', big: '#' },
+  },
+  {
+    row: 1, code: 'Digit4', spec: false, ru: { capstyle: false, sml: '4', big: ';' }, en: { capstyle: false, sml: '4', big: '$' },
+  },
+  {
+    row: 1, code: 'Digit5', spec: false, ru: { capstyle: false, sml: '5', big: '%' }, en: { capstyle: false, sml: '5', big: '%' },
+  },
+  {
+    row: 1, code: 'Digit6', spec: false, ru: { capstyle: false, sml: '6', big: ':' }, en: { capstyle: false, sml: '6', big: '^' },
+  },
+  {
+    row: 1, code: 'Digit7', spec: false, ru: { capstyle: false, sml: '7', big: '?' }, en: { capstyle: false, sml: '7', big: '&' },
+  },
+  {
+    row: 1, code: 'Digit8', spec: false, ru: { capstyle: false, sml: '8', big: '*' }, en: { capstyle: false, sml: '8', big: '*' },
+  },
+  {
+    row: 1, code: 'Digit9', spec: false, ru: { capstyle: false, sml: '9', big: '(' }, en: { capstyle: false, sml: '9', big: '(' },
+  },
+  {
+    row: 1, code: 'Digit0', spec: false, ru: { capstyle: false, sml: '0', big: ')' }, en: { capstyle: false, sml: '0', big: ')' },
+  },
+  {
+    row: 1, code: 'Minus', spec: true, ru: { capstyle: false, sml: '-', big: '_' }, en: { capstyle: false, sml: '-', big: '_' },
+  },
+  {
+    row: 1, code: 'Equal', spec: true, ru: { capstyle: false, sml: '=', big: '+' }, en: { capstyle: false, sml: '=', big: '+' },
+  },
+  {
+    row: 1, code: 'Backspace', spec: true, ru: { capstyle: false, sml: '⌫ Backspace', big: '⌫ Backspace' }, en: { capstyle: false, sml: '⌫ Backspace', big: '⌫ Backspace' },
+  },
+  {
+    row: 2, code: 'Tab', spec: true, ru: { capstyle: false, sml: 'Tab ↹', big: 'Tab ↹' }, en: { capstyle: false, sml: 'Tab ↹', big: 'Tab ↹' },
+  },
+  {
+    row: 2, code: 'KeyQ', spec: false, ru: { capstyle: true, sml: 'й', big: 'Й' }, en: { capstyle: true, sml: 'q', big: 'Q' },
+  },
+  {
+    row: 2, code: 'KeyW', spec: false, ru: { capstyle: true, sml: 'ц', big: 'Ц' }, en: { capstyle: true, sml: 'w', big: 'W' },
+  },
+  {
+    row: 2, code: 'KeyE', spec: false, ru: { capstyle: true, sml: 'у', big: 'У' }, en: { capstyle: true, sml: 'e', big: 'E' },
+  },
+  {
+    row: 2, code: 'KeyR', spec: false, ru: { capstyle: true, sml: 'к', big: 'К' }, en: { capstyle: true, sml: 'r', big: 'R' },
+  },
+  {
+    row: 2, code: 'KeyT', spec: false, ru: { capstyle: true, sml: 'е', big: 'Е' }, en: { capstyle: true, sml: 't', big: 'T' },
+  },
+  {
+    row: 2, code: 'KeyY', spec: false, ru: { capstyle: true, sml: 'н', big: 'Н' }, en: { capstyle: true, sml: 'y', big: 'Y' },
+  },
+  {
+    row: 2, code: 'KeyU', spec: false, ru: { capstyle: true, sml: 'г', big: 'Г' }, en: { capstyle: true, sml: 'u', big: 'U' },
+  },
+  {
+    row: 2, code: 'KeyI', spec: false, ru: { capstyle: true, sml: 'ш', big: 'Ш' }, en: { capstyle: true, sml: 'i', big: 'I' },
+  },
+  {
+    row: 2, code: 'KeyO', spec: false, ru: { capstyle: true, sml: 'щ', big: 'Щ' }, en: { capstyle: true, sml: 'o', big: 'O' },
+  },
+  {
+    row: 2, code: 'KeyP', spec: false, ru: { capstyle: true, sml: 'з', big: 'З' }, en: { capstyle: true, sml: 'p', big: 'P' },
+  },
+  {
+    row: 2, code: 'BracketLeft', spec: false, ru: { capstyle: true, sml: 'х', big: 'Х' }, en: { capstyle: false, sml: '[', big: '{' },
+  },
+  {
+    row: 2, code: 'BracketRight', spec: false, ru: { capstyle: true, sml: 'ъ', big: 'Ъ' }, en: { capstyle: false, sml: ']', big: '}' },
+  },
+  {
+    row: 2, code: 'Backslash', spec: false, ru: { capstyle: false, sml: '\\', big: '/' }, en: { capstyle: false, sml: '\\', big: '|' },
+  },
+  {
+    row: 3, code: 'CapsLock', spec: true, ru: { capstyle: false, sml: 'CapsLock', big: 'CapsLock' }, en: { capstyle: false, sml: 'CapsLock', big: 'CapsLock' },
+  },
+  {
+    row: 3, code: 'KeyA', spec: false, ru: { capstyle: true, sml: 'ф', big: 'Ф' }, en: { capstyle: true, sml: 'a', big: 'A' },
+  },
+  {
+    row: 3, code: 'KeyS', spec: false, ru: { capstyle: true, sml: 'ы', big: 'Ы' }, en: { capstyle: true, sml: 's', big: 'S' },
+  },
+  {
+    row: 3, code: 'KeyD', spec: false, ru: { capstyle: true, sml: 'в', big: 'В' }, en: { capstyle: true, sml: 'd', big: 'D' },
+  },
+  {
+    row: 3, code: 'KeyF', spec: false, ru: { capstyle: true, sml: 'а', big: 'А' }, en: { capstyle: true, sml: 'f', big: 'F' },
+  },
+  {
+    row: 3, code: 'KeyG', spec: false, ru: { capstyle: true, sml: 'п', big: 'П' }, en: { capstyle: true, sml: 'g', big: 'G' },
+  },
+  {
+    row: 3, code: 'KeyH', spec: false, ru: { capstyle: true, sml: 'р', big: 'Р' }, en: { capstyle: true, sml: 'h', big: 'H' },
+  },
+  {
+    row: 3, code: 'KeyJ', spec: false, ru: { capstyle: true, sml: 'о', big: 'О' }, en: { capstyle: true, sml: 'j', big: 'J' },
+  },
+  {
+    row: 3, code: 'KeyK', spec: false, ru: { capstyle: true, sml: 'л', big: 'Л' }, en: { capstyle: true, sml: 'k', big: 'K' },
+  },
+  {
+    row: 3, code: 'KeyL', spec: false, ru: { capstyle: true, sml: 'д', big: 'Д' }, en: { capstyle: true, sml: 'l', big: 'L' },
+  },
+  {
+    row: 3, code: 'Semicolon', spec: false, ru: { capstyle: true, sml: 'ж', big: 'Ж' }, en: { capstyle: false, sml: ';', big: ':' },
+  },
+  {
+    row: 3, code: 'Quote', spec: false, ru: { capstyle: true, sml: 'э', big: 'Э' }, en: { capstyle: false, sml: '\'', big: '"' },
+  },
+  {
+    row: 3, code: 'Enter', spec: true, ru: { capstyle: false, sml: '↵ Enter', big: '↵ Enter' }, en: { capstyle: false, sml: '↵ Enter', big: '↵ Enter' },
+  },
+  {
+    row: 4, code: 'ShiftLeft', spec: true, ru: { capstyle: false, sml: 'Shift ⇧', big: 'Shift ⇧' }, en: { capstyle: false, sml: 'Shift ⇧', big: 'Shift ⇧' },
+  },
+  {
+    row: 4, code: 'KeyZ', spec: false, ru: { capstyle: true, sml: 'я', big: 'Я' }, en: { capstyle: true, sml: 'z', big: 'Z' },
+  },
+  {
+    row: 4, code: 'KeyX', spec: false, ru: { capstyle: true, sml: 'ч', big: 'Ч' }, en: { capstyle: true, sml: 'x', big: 'X' },
+  },
+  {
+    row: 4, code: 'KeyC', spec: false, ru: { capstyle: true, sml: 'с', big: 'С' }, en: { capstyle: true, sml: 'c', big: 'C' },
+  },
+  {
+    row: 4, code: 'KeyV', spec: false, ru: { capstyle: true, sml: 'м', big: 'М' }, en: { capstyle: true, sml: 'v', big: 'V' },
+  },
+  {
+    row: 4, code: 'KeyB', spec: false, ru: { capstyle: true, sml: 'и', big: 'И' }, en: { capstyle: true, sml: 'b', big: 'B' },
+  },
+  {
+    row: 4, code: 'KeyN', spec: false, ru: { capstyle: true, sml: 'т', big: 'Т' }, en: { capstyle: true, sml: 'n', big: 'N' },
+  },
+  {
+    row: 4, code: 'KeyM', spec: false, ru: { capstyle: true, sml: 'ь', big: 'Ь' }, en: { capstyle: true, sml: 'm', big: 'M' },
+  },
+  {
+    row: 4, code: 'Comma', spec: false, ru: { capstyle: true, sml: 'б', big: 'Б' }, en: { capstyle: false, sml: '.', big: '<' },
+  },
+  {
+    row: 4, code: 'Period', spec: false, ru: { capstyle: true, sml: 'ю', big: 'Ю' }, en: { capstyle: false, sml: ',', big: '>' },
+  },
+  {
+    row: 4, code: 'Slash', spec: false, ru: { capstyle: false, sml: '.', big: ',' }, en: { capstyle: false, sml: '/', big: '?' },
+  },
+  {
+    row: 4, code: 'ArrowUp', spec: false, ru: { capstyle: false, sml: '↑', big: '↑' }, en: { capstyle: false, sml: '↑', big: '↑' },
+  },
+  {
+    row: 4, code: 'ShiftRight', spec: true, ru: { capstyle: false, sml: '⇧ Shift', big: '⇧ Shift' }, en: { capstyle: false, sml: '⇧ Shift', big: '⇧ Shift' },
+  },
+  {
+    row: 5, code: 'ControlLeft', spec: true, ru: { capstyle: false, sml: 'Ctrl', big: 'Ctrl' }, en: { capstyle: false, sml: 'Ctrl', big: 'Ctrl' },
+  },
+  {
+    row: 5, code: 'AltLeft', spec: true, ru: { capstyle: false, sml: 'Alt', big: 'Alt' }, en: { capstyle: false, sml: 'Alt', big: 'Alt' },
+  },
+  {
+    row: 5, code: 'Space', spec: true, ru: { capstyle: false, sml: ' ', big: ' ' }, en: { capstyle: false, sml: ' ', big: ' ' },
+  },
+  {
+    row: 5, code: 'AltRight', spec: true, ru: { capstyle: false, sml: 'Alt', big: 'Alt' }, en: { capstyle: false, sml: 'Alt', big: 'Alt' },
+  },
+  {
+    row: 5, code: 'Delete', spec: true, ru: { capstyle: false, sml: 'Del', big: 'Del' }, en: { capstyle: false, sml: 'Del', big: 'Del' },
+  },
+  {
+    row: 5, code: 'ArrowLeft', spec: false, ru: { capstyle: false, sml: '←', big: '←' }, en: { capstyle: false, sml: '←', big: '←' },
+  },
+  {
+    row: 5, code: 'ArrowDown', spec: false, ru: { capstyle: false, sml: '↓', big: '↓' }, en: { capstyle: false, sml: '↓', big: '↓' },
+  },
+  {
+    row: 5, code: 'ArrowRight', spec: false, ru: { capstyle: false, sml: '→', big: '→' }, en: { capstyle: false, sml: '→', big: '→' },
+  },
+  {
+    row: 5, code: 'ControlRight', spec: true, ru: { capstyle: false, sml: 'Ctrl', big: 'Ctrl' }, en: { capstyle: false, sml: 'Ctrl', big: 'Ctrl' },
+  },
+
+];
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+import Keyboard from './keyboard.js';
+
+window.onload = () => {
+  const kbrd = new Keyboard();
+  kbrd.init();
+};
